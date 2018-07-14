9ed11a9d02799cec94bc857d059f3a3396bed1ec
gnans <gnanasekaranpg@gmail.com>
2018-07-14 12:33:26 +0530

    value select popup added

--- www/js/game-play-board.js
+++ www/js/game-play-board.js
@@ -1,10 +1,12 @@
+var sign, id, select_score;
+     
 $(document).ready(function(){
 
     // window.sessionStorage.setItem("checked", "false");
 
 
-      // var is_aggressive = $('#cmn-toggle-4').is(':checked') ? 'y': 'n';
-      $("#cmn-toggle-4").attr("checked", window.sessionStorage.getItem("checked") == 'true');
+	  // var is_aggressive = $('#cmn-toggle-4').is(':checked') ? 'y': 'n';
+	  $("#cmn-toggle-4").attr("checked", window.sessionStorage.getItem("checked") == 'true');
 
 
     $('#cmn-toggle-4').change(function(){
@@ -14,21 +16,39 @@ $(document).ready(function(){
     var score1 = window.sessionStorage.getItem("1") ? window.sessionStorage.getItem("1") : 0;
     var score2 = window.sessionStorage.getItem("2") ? window.sessionStorage.getItem("2") : 0;
 
-    $("input[name=" + "'quant[1]']").val(score1);
-	$("input[name=" + "'quant[2]']").val(score2);
+    $("input[name=1]").val(score1);
+	$("input[name=2]").val(score2);
 
-	$(".input-group select").hide();
-	
-	$(".input-group .input-number").click(function() {
-		$(".input-group select[name='" + $(this).attr('name')+"']").toggle();
-		$("input[name='" + $(this).attr('name') + "']").toggle();
+	$(".input-group .btn-number").click(function() {
+		sign = $(this).data('type');
+		id = $(this).data('field');
+		valuesSheet();
 	});
 });
 
-function updateScore(sign, id) {
-	var initial;
-	var val = $(".input-group select[name='quant["+id+"]']").val();
+var callback = function(buttonIndex) {
+    setTimeout(function() {
+	  	if (buttonIndex <= 2) {
+	 		select_score = buttonIndex == 1 ? 1 : 0.5;	 	
+	 		updateScore(sign, id, select_score)
+	 	}
+    });
+};
 
+function valuesSheet() {
+	var options = {
+		title: 'Select value',
+		buttonLabels: ['1', '0.5'],
+		addCancelButtonWithLabel: 'Cancel',
+		androidEnableCancelButton : true,
+		winphoneEnableCancelButton : true,
+	};
+	window.plugins.actionsheet.show(options, callback);
+}
+
+function updateScore(sign, id, select_score) {
+	var initial;
+	
 	if(!window.sessionStorage.getItem("initial")) {
 		initial = true;
 		window.sessionStorage.setItem("initial", "false");
@@ -38,11 +58,10 @@ function updateScore(sign, id) {
 
 
 	$.ajax({
-		url: 'http://45.79.7.27:81/corkcup/team/updateScore.php?id=' +id+ "&sign=" + sign + "&initial=" + initial + "&select_score=" + val,
+		url: 'http://45.79.7.27:81/corkcup/team/updateScore.php?id=' +id+ "&sign=" + sign + "&initial=" + initial + "&select_score=" + select_score,
 		success: function(result) {
 			if(result){
-				$("input[name=" + "'quant[" + result.id + "]']").val(result.scores);
-				$("input[name=" + "'quant[" + result.id + "]']").trigger('click');
+				$("input[name=" + "'" + result.id + "']").val(result.scores);
 				window.sessionStorage.setItem(result.id, result.scores);
 			}
 		}, 
